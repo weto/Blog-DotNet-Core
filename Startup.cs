03c13793334c27a4c30aee71b11d881167e35604
Wellington Jesus da Conceição <bi002100@bhz-mt-002593.intermedium.local>
2019-09-17 10:40:37 -0300

    Add swagger do tipo Swashbuckle.AspNetCore

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,12 @@
-﻿using EFGetStarted.AspNetCore.NewDb.Models;
+﻿using System;
+using EFGetStarted.AspNetCore.NewDb.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 namespace modelo_2
 {
@@ -20,7 +22,29 @@ namespace modelo_2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo 
+                { 
+                    Version = "v1",
+                    Title = "Meu primeiro microsservicço",
+                    Description = "O primeiro exemplo da primeira API em dotnet core.",
+                    TermsOfService = new Uri("http://localhost:8080/api/blogs"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Wellington",
+                        Email = string.Empty,
+                        Url = new Uri("https://twitter.com")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://teste.com/license"),
+                    }
+                });
+            });
 
             services.Configure<CookiePolicyOptions> (Options => {
                 Options.CheckConsentNeeded = context => true;
@@ -46,6 +70,11 @@ namespace modelo_2
                 app.UseHsts();
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Meu primeiro microsserviço V1");
+                c.RoutePrefix = string.Empty;
+            });
             app.UseHttpsRedirection();
             app.UseMvc();
             UpdateDatabase(app);
